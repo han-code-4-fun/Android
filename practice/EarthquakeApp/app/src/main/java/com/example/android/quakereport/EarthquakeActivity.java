bb520ffe0e8b739b91f5a5b7f3ffdefbdce8fac3
miaonagemide <hanzhou244@gmail.com>
2018-12-30 17:58:32 -0800

    feat: add menu and relative fragment, activity and xml

--- practice/EarthquakeApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ practice/EarthquakeApp/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -25,6 +25,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -160,6 +162,23 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     public void onLoaderReset(Loader<List<EarthquakeInfo>> loader) {
         Log.i("onLoaderReset","reset loader");
         myAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // old version AsyncTask
