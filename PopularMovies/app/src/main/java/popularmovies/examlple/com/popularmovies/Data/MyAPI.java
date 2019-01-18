2e12859558c633355a32b19f3beac211be99595a
miaonagemide <hanzhou244@gmail.com>
2019-01-17 23:05:10 -0800

    docs: upgrade gradle

---
+++ PopularMovies/app/src/main/java/popularmovies/examlple/com/popularmovies/Data/MyAPI.java
@@ -0,0 +1,8 @@
+package popularmovies.examlple.com.popularmovies.Data;
+
+public final class MyAPI {
+    //todo put my API key, before I upload, hide this part
+    //todo use dummy data first
+    public final String API_KEY = "";
+
+}
---
+++ PopularMovies/app/src/main/java/popularmovies/examlple/com/popularmovies/Data/QueryUtils.java
@@ -0,0 +1,14 @@
+package popularmovies.examlple.com.popularmovies.Data;
+
+import android.content.Context;
+
+import com.squareup.picasso.Picasso;
+
+public final class QueryUtils {
+    public static void getImage(String uri)
+    {
+        Picasso.get();
+
+
+    }
+}
