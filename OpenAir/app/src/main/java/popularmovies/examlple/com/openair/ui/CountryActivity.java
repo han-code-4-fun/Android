fa2510bf505e87c0793743973ca9c9b125a446af
miaonagemide <hanzhou244@gmail.com>
2019-01-20 01:11:28 -0800

    test: test correctness of URL output from AQURL class

--- OpenAir/app/src/main/java/popularmovies/examlple/com/openair/ui/CountryActivity.java
+++ OpenAir/app/src/main/java/popularmovies/examlple/com/openair/ui/CountryActivity.java
@@ -2,14 +2,12 @@ package popularmovies.examlple.com.openair.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import popularmovies.examlple.com.openair.R;
+import utils.AQURL;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
-
-import java.util.concurrent.TimeUnit;
+import android.widget.TextView;
 
 public class CountryActivity extends AppCompatActivity {
     Spinner countries,cities;
@@ -25,6 +23,13 @@ public class CountryActivity extends AppCompatActivity {
         cities = findViewById(R.id.spinner_city);
         submit = findViewById(R.id.btn_submit);
 
+
+        /*test URL
+        TextView title = findViewById(R.id.tv_country_title);
+        title.setTextSize(20);
+        title.setAllCaps(false);
+        title.setText(AQURL.getDetailMeasurementURL("CN","台州市", "台州环保大楼").toString());
+        */
 
     }
 }
