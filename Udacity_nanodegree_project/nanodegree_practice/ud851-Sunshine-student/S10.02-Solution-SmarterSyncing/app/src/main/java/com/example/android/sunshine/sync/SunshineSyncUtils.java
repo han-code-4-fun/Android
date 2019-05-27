6609431e737516537e1cb9367649600716ceb332
miaonagemide <hanzhou244@gmail.com>
2019-05-27 06:50:38 -0700

    practice

--- Udacity_nanodegree_project/nanodegree_practice/ud851-Sunshine-student/S10.02-Solution-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ Udacity_nanodegree_project/nanodegree_practice/ud851-Sunshine-student/S10.02-Solution-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -108,12 +108,12 @@ public class SunshineSyncUtils {
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
-     * execution.
-     *
+                    * execution.
+                    *
      * @param context The Context used to start the IntentService for the sync.
      */
-    public static void startImmediateSync(@NonNull final Context context) {
-        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
-        context.startService(intentToSyncImmediately);
+            public static void startImmediateSync(@NonNull final Context context) {
+                Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+                context.startService(intentToSyncImmediately);
     }
 }
